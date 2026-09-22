be13213e3a2354edd764fedbd8f2d24418b77e2d
Igor Nicoletti <igor93nicoletti@gmail.com>
2026-09-22 04:10:56 -0300

    refactor: complete app page layout migration

--- src/pages/users/users.layout.tsx
+++ src/pages/users/users.layout.tsx
@@ -1,6 +1,6 @@
 import { appPages } from "@/app/app-config"
-import { AppPage } from "@/components/common/app-page"
+import { AppPageLayout } from "@/app/layouts/app-page-layout"
 
 export function UsersPage() {
-  return <AppPage page={appPages["users"]} />
+  return <AppPageLayout page={appPages["users"]} />
 }
